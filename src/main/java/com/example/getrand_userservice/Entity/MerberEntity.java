53d651c4f8edb5e7b91f08c6f5c19913e3e50a1f
CoderJDan <coder.jangdaniel@gmail.com>
2024-12-21 15:12:18 +0900

    12/20 user service update

---
+++ src/main/java/com/example/getrand_userservice/Entity/MerberEntity.java
@@ -0,0 +1,30 @@
+package com.example.getrand_userservice.Entity;
+
+import com.example.getrand_userservice.dto.UsersDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "users")
+public class MerberEntity {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column
+    private String username;
+    @Column(unique = true)
+    private String useremail;
+    @Column
+    private String password;
+
+    public static MerberEntity tomemberEntity(UsersDTO usersDTO) {
+        MerberEntity merberEntity = new MerberEntity();
+        merberEntity.setUsername(usersDTO.getUsername());
+        merberEntity.setUseremail(usersDTO.getUseremail());
+        merberEntity.setPassword(usersDTO.getPassword());
+        return merberEntity;
+    }
+}
---
+++ src/main/java/com/example/getrand_userservice/dto/UsersDTO.java
@@ -0,0 +1,26 @@
+package com.example.getrand_userservice.dto;
+
+import com.example.getrand_userservice.Entity.MerberEntity;
+import lombok.*;
+import org.apache.catalina.User;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UsersDTO {
+    private long userid;
+    private String username;
+    private String useremail;
+    private String password;
+
+    public static UsersDTO toUsersDTO(User user) {
+        UsersDTO userDTO = new UsersDTO();
+        userDTO.setUserid(MerberEntity.getId());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setUseremail(userDTO.getUseremail());
+        userDTO.setPassword(user.getPassword());
+        return userDTO;
+    }
+}
