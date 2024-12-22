d67f21e47d7de45fd10e2780100afe4432e6e5a9
Hyeoncoding <hyeonjinj1440@gmail.com>
2024-12-23 01:51:07 +0900

    실행 오류 파일 주석 처리 yml 파일 수정(name 변경)

--- src/main/java/com/example/getrand_userservice/Repository/MerberRepository.java
+++ src/main/java/com/example/getrand_userservice/Repository/MerberRepository.java
@@ -1,10 +1,10 @@
-package com.example.getrand_userservice.Repository;
-
-import com.example.getrand_userservice.Entity.MerberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MerberRepository extends JpaRepository<MerberEntity, Long> {
-    Optional<MerberEntity> findMerberByUsername(String username);
-}
+//package com.example.getrand_userservice.Repository;
+//
+//import com.example.getrand_userservice.Entity.MerberEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface MerberRepository extends JpaRepository<MerberEntity, Long> {
+//    Optional<MerberEntity> findMerberByUsername(String username);
+//}
--- src/main/java/com/example/getrand_userservice/serivce/MerberService.java
+++ src/main/java/com/example/getrand_userservice/serivce/MerberService.java
@@ -1,39 +1,39 @@
-package com.example.getrand_userservice.serivce;
-
-import com.example.getrand_userservice.Entity.MerberEntity;
-import com.example.getrand_userservice.Repository.MerberRepository;
-import com.example.getrand_userservice.dto.UsersDTO;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class MerberService {
-    private final MerberRepository merberRepository;
-
-    // 회원 저장
-    public void save(UsersDTO usersDTO) {
-        MerberEntity merberEntity = MerberEntity.tomemberEntity(usersDTO);
-        merberRepository.save(merberEntity);
-    }
-
-    // 로그인 처리
-    public UsersDTO login(UsersDTO usersDTO) {
-        Optional<MerberEntity> byUsername = merberRepository.findMerberByUsername(usersDTO.getUsername());
-
-        if (byUsername.isPresent()) {
-            MerberEntity merberEntity = byUsername.get();
-
-            // 비밀번호 검증
-            if (merberEntity.getPassword().equals(usersDTO.getPassword())) {
-                return merberEntity.tomemberEntity(); // Entity를 DTO로 변환
-            } else {
-                return null; // 비밀번호 불일치
-            }
-        }
-
-        return null; // 사용자 이름 불일치
-    }
-}
+//package com.example.getrand_userservice.serivce;
+//
+//import com.example.getrand_userservice.Entity.MerberEntity;
+//import com.example.getrand_userservice.Repository.MerberRepository;
+//import com.example.getrand_userservice.dto.UsersDTO;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class MerberService {
+//    private final MerberRepository merberRepository;
+//
+//    // 회원 저장
+//    public void save(UsersDTO usersDTO) {
+//        MerberEntity merberEntity = MerberEntity.tomemberEntity(usersDTO);
+//        merberRepository.save(merberEntity);
+//    }
+//
+//    // 로그인 처리
+//    public UsersDTO login(UsersDTO usersDTO) {
+//        Optional<MerberEntity> byUsername = merberRepository.findMerberByUsername(usersDTO.getUsername());
+//
+//        if (byUsername.isPresent()) {
+//            MerberEntity merberEntity = byUsername.get();
+//
+//            // 비밀번호 검증
+//            if (merberEntity.getPassword().equals(usersDTO.getPassword())) {
+//                return merberEntity.tomemberEntity(); // Entity를 DTO로 변환
+//            } else {
+//                return null; // 비밀번호 불일치
+//            }
+//        }
+//
+//        return null; // 사용자 이름 불일치
+//    }
+//}
